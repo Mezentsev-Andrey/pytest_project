d5b4f3001edfb6f86bd0a619b94dce3cfa7931ba
Andrey Mezentsev <mezentsew@mail.ru>
2023-11-05 12:55:51 +0300

    Add all calculate_tax functions in taxes module.

--- tests/test_taxes.py
+++ tests/test_taxes.py
@@ -26,19 +26,51 @@ def test_calculate_taxes_invalid_price():
                                                 (50, 5, 52.5),
                                                 (100, 0, 100)
                                                 ])
-
 def test_calculate_tax(price, tax_rate, expected):
     assert calculate_tax(price, tax_rate) == expected
 
 
 @pytest.mark.parametrize("price", [- 100, 0])
-
 def test_calculate_tax_invalid_price(price):
     with pytest.raises(ValueError):
         calculate_tax(price, 10)
 
 @pytest.mark.parametrize("tax_rate", [- 100, 100, 101])
-
 def test_calculate_tax_invalid_tax(tax_rate):
     with pytest.raises(ValueError):
         calculate_tax(100, tax_rate)
+
+@pytest.mark.parametrize("discount, expected", [(0, 110),
+                                                (10, 99),
+                                                (100, 0)
+                                                ])
+def test_calculate_tax_with_discount(discount, expected):
+    assert calculate_tax(100, 10, discount=discount) == expected
+
+
+def test_calculate_tax_with_no_discount():
+    assert calculate_tax(100, 10) == 110.0
+
+
+@pytest.mark.parametrize("round_digits, expected", [(0, 99),
+                                                (1, 99.4),
+                                                (2, 99.42),
+                                                (3, 99.425),
+                                                ])
+def test_calculate_tax_with_round_digits(round_digits, expected):
+    assert calculate_tax(100, 2.5, discount=3, round_digits=round_digits) == expected
+
+
+@pytest.mark.parametrize("price, tax_rate, discount, round_digits",
+                                                    [('100', 2.5, 3 , 1),
+                                                    (100, '2.5', 3 , 1),
+                                                    (100, 2.5, '3' , 1),
+                                                    (100, 2.5, 3 , '1'),
+                                                    ])
+def test_calculate_tax_with_round_digits(price, tax_rate, discount, round_digits):
+    with pytest.raises(TypeError):
+        calculate_tax(price, tax_rate, discount=discount, round_digits=round_digits)
+
+def test_calculate_tax_with_round_digits():
+    with pytest.raises(TypeError):
+        calculate_tax(100, 10, 5, 2)
